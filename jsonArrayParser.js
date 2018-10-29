3d179139915186955afeea7813b965f592576646
K. Lee <38235501+cocoa-salon@users.noreply.github.com>
2018-10-29 11:22:07 +0900

    Lec4 Array Parser 커밋 로그 step2 (#92)
    * Lec4 Array Parser 커밋 로그 step2
    
    -프로그램을 설계하는 과정에서 무한중첩 배열도 분석이 가능한 것으로 보여 좀더 디벨롭하였습니다.
    
    -각각의 배열 데이터에 대해 깊이(depth) 속성을 추가하여 가독성을 향상시켰습니다.
    
    -문자열을 해체하는 메소드를 다양한 유형의 문자열 처리에 대응하는 세부 메소드로 분리하는 일이 매끄럽지 않아 몇 번의 시도 끝에 원래대로 두었습니다. 미흡한 점에 대해 추후 피드백 시 최우선으로 반영하겠습니다.
    
    감사합니다.
    
    * Lec4 Array Parser 커밋 로그 step2 modified 1

--- jsonArrayParser.js
+++ jsonArrayParser.js
@@ -1,43 +1,75 @@
-// arrayParser step1
+// arrayParser step2(2중 중첩 배열 분석) modified 1
 
 const arrayParser = {
 
-    result: {
-        child: []
+    // 분석된 문자열 데이터(줄기)
+    stem: [],
+
+    // 배열의 깊이(-1부터 시작)
+    depth: -1,
+
+
+    checkDataType(targetStr, ch) {
+        if (!(ch === "[" && targetStr[0] === '[' && targetStr[targetStr.length - 1] === ']')) return;
+        this.depth++;
+        this.stem[this.depth] = {};
+        this.stem[this.depth].child = [];
+        this.stem[this.depth].type = 'array';
+        this.stem[this.depth].value = 'ArrayObject';
+
     },
 
-    getResultAnalyzed(str) {
-        this.checkStrType(str);
-        this.extractNumber(str);
-        return this.result;
-    },
+    // 문자열 해체 메소드
+    getAnalyzedStem(targetStr) {
+        let extractedNum = "";
+        for (let ch of targetStr) {
+            
+            this.checkDataType(targetStr, ch);
 
-    checkStrType(str) {
-        if (str[0] === '[' && str[str.length - 1] === ']')
-            this.result.type = 'Array';
-    },
+            if (!isNaN(Number(ch))) { // 숫자형식의 문자열을 인식하여 추출
+                extractedNum += ch;
+                targetStr = targetStr.replace(ch, "");
 
-    extractNumber(str) {
-        let stringNum = "";
-        for (let ele of str) {
-            if (!isNaN(Number(ele))) stringNum += ele;
-            if (ele === "," || ele === "]") {
-                this.analyzeStrNum(stringNum);
-                stringNum = "";
+            } else if (ch === "," && extractedNum !== "") { // 분석할 문자열을 추출한 경우 ','를 만나면 분석 메소드 호출
+                this.analyzeStr(extractedNum);
+                extractedNum = "";
+
+            } else if (ch === "]" && extractedNum !== "") { // 분석할 문자열을 추출한 경우 ']'를 만나면 분석 메소드 호출
+                this.analyzeStr(extractedNum);
+                extractedNum = "";
+                this.depth--;
+
+            } else if (ch === "]" && extractedNum === "") { // 분석할 문자열이 없는 경우 : ex( ]] )
+                this.depth--;
             }
         }
+        return this.printArrayStem(this.stem); // 분석 결과 출력 메소드
     },
 
-    analyzeStrNum(stringNum) {
+    // 추출 문자열 분석 메소드
+    analyzeStr(extractedNum) {
         const childObj = {};
-        childObj.type = typeof Number(stringNum);
-        childObj.value = stringNum;
+        childObj.type = typeof Number(extractedNum);
+        childObj.value = extractedNum;
         childObj.child = [];
-        this.result.child.push(childObj);
+        this.stem[this.depth].child.push(childObj);
     },
+
+    // 분석 결과 출력 메소드
+    printArrayStem(stem) {
+        for (let i = stem.length - 1; i > 0; i--) {
+            stem[i - 1].child.push(stem[i]);
+        }
+        return stem;
+    }
 
 } // end arrayParser 
 
-const str = '[12,345,6789]';
-let result = arrayParser.getResultAnalyzed(str);
-console.log(JSON.stringify(result, null, 2));
+// 분석 대상 문자열 데이터
+let targetStr = '[123,[22,23,[11,[112233],112],55],33]]';
+//let targetStr = '[1,2,3,4,5,[6]]'
+//let targetStr = '[10,[20,30,[40,50,60,70]]]';
+//let targetStr = '[10,[20,[30,[40,[50,[60,[70,[80,[90,[100]]]]]]]]]]';
+
+let analyzedStem = arrayParser.getAnalyzedStem(targetStr)[0];
+console.log(JSON.stringify(analyzedStem, null, 4));
