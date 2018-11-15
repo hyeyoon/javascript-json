e3d2a197208363b57f909c8dc24c321f40d70d66
Head <40166539+HTMLhead@users.noreply.github.com>
2018-11-15 14:30:19 +0900

    lec4-Step4.여러가지 Type (#111)
    * 설계용 파일 작성, errorCheck함수만듬
    
    * 설계완료
    
    * 타입체크 완료
    
    * 불리언 타입일때의 제이슨 데이터 생성
    
    * Null타입일때의 제이슨 데이터 생성
    
    * string타입일때의 제이슨 데이터 생성
    
    * 모든 상황에서의 continue추가. 또 Number를 집어넣으면 안되니..
    
    * 설계변경
    
    * 에러체크 클래스 제작
    
    * 결론적으로 사용하는 클래스들의 사용법 정리
    
    * 각 함수들의 껍데기 완성
    
    * 문자열의 에러를 체크하는 함수 checkString완성
    
    * errorcheck클래스의 checkString메서드 완성
    
    * null과 boolean은 필요가 없겠다고 생각되어 제거
    
    * checkError클래스의 checkNumber의 껍데기를 다시 만듬
    
    * checkNumber함수 완성
    
    * checkNumber를 typeof 로 타입을 판별하는것을 isNaN으로 판별하도록 변경
    
    * getChild함수에 checkError를 집어넣음.
    
    * checkErorr => errorCheck
    
    * errorCheck의 모든 인자에 checkingValue를 넣어줌
    
    * 여백의 칸 정리

--- jsonParser.js
+++ jsonParser.js
@@ -6,7 +6,7 @@ class JSONData {
     }
 }
 
-const sentence = "[123,[22,[33]],44,[1,2,3],11]".replace(/ /gi, '')
+const sentence = "['1a'3',[22,23,[11,[112233],112],55],33]".replace(/ /gi, '')
 
 class Tokenize {
     constructor() {
@@ -36,8 +36,9 @@ class Tokenize {
 }
 
 class Analyze {
-    constructor(queue) {
+    constructor(queue, errorCheck) {
         this.queueArr = queue
+        this.errorCheck = errorCheck
     }
     
     queue() {
@@ -61,10 +62,50 @@ class Analyze {
                 continue;
             } else if (checkingValue === ']') {
                 break;
+            } else if (checkingValue === 'true' || checkingValue === 'false') {
+                child.push(new JSONData('Boolean', checkingValue, []))
+                continue;
+            } else if (checkingValue === 'null') {
+                child.push(new JSONData('Null', checkingValue, []))
+                continue;
+            } else if (checkingValue[0] === "'") {
+                if(this.errorCheck.checkString(checkingValue)) {
+                    console.log(`${checkingValue}는 제대로된 문자열이 아닙니다.`)
+                    return 
+                }
+                child.push(new JSONData('String', checkingValue, []))
+                continue;
+            }
+            if(this.errorCheck.checkNumber(checkingValue)) {
+                console.log(`${checkingValue}은 알수없는 문자열입니다.`)
+                return
             }
             child.push(new JSONData('Number', checkingValue, []))
         }
         return child
+    }
+}
+
+class ErrorCheck {    
+    checkString(token) {
+        debugger;
+        let count = 0
+        for(let position = 0; position < token.length; position++) {
+            if(token[position] === "'") {
+                count++
+            }
+        }
+        if(count === 2 && token[0] === "'" && token[token.length-1] === "'") {
+            return false
+        }
+        return true
+    }
+
+    checkNumber(token) {
+        if(isNaN(Number(token)) === true) {
+            return true
+        }
+        return false
     }
 }
 
@@ -74,6 +115,8 @@ function printJSONData(JSONData) {
 
 const tokenize = new Tokenize
 const tokenizedDataArr = tokenize.getWholeDataQueue(sentence)
-const analyze = new Analyze(tokenizedDataArr)
+const errorCheck = new ErrorCheck
+const analyze = new Analyze(tokenizedDataArr, errorCheck)
 const jsonData = analyze.queue()
 printJSONData(jsonData)
+
