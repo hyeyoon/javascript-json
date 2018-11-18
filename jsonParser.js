e3e980a8230c97a646069bb2db5054e2c47ba6a5
Head <40166539+HTMLhead@users.noreply.github.com>
2018-11-19 00:10:19 +0900

    lec4-Step5.객체 Type (#115)
    * isNaN(Number(token))) === true 의 === 와 true제거
    
    * for문을 for of문으로 변경, count => quotesCount
    
    * jsondata를 만들때 object타입일때를 대비하여 keyValue값 추가
    
    * 설계파일 생성
    
    * 설계 완성
    
    * JSONdata class의 keyvalue값 제거
    
    * getToken함수에 {값과 :값, }값을 통해 원하는 대로 token화 시킬수 있도록 만듬
    
    * getChild 에서 {값이 주어진다면 만드는 값 생성
    
    * :값이 포함되어 있는 값이 주어진다면 key값으로 만드는 조건문 완성
    
    * }값이 나오면 반복문 탈출하도록 조건문 완성
    
    * 요구사항을 체크하며 수행하기 위해 주석처리한 요구사항을 만듬
    
    * 마지막 함수를 변수는 짧게 함수 이름은 길게 만듬
    
    * queue함수수정시작
    
    * queue메서드 내에 필요한 메서드 makeArrayChild, makeObjectChild껍데기 제작
    
    * makeArrayChild함수 완성
    
    * queue 함수에 적용
    
    * makeObjectChild함수 완성
    
    * queue에 적용
    
    * getChild함수 수정시작, checkingValue가 }나 ]일때 같은 동작을 하도록 수정
    
    * analyze 객체 수정 일단락
    
    * conutQueueNum함수 제작
    
    * countQueueNum함수 완성, checkString에 적용
    
    * this 추가
    
    * errorCheck 객체 수정 일단락
    
    * analyze객체 수정 시작
    
    * analyze말고 errorcheck객체에서 해결해야하는 문제임을 깨닫고, errorCheck객체 수정 시작
    
    * printErrorMessage 함수 생성
    
    * printErrorMessage 함수 생성
    
    * checkString 메서드와 checkNumber메서드에 printErrorMessage메서드 적용
    
    * token 추가
    
    * analyze객내의 getChild함수에 checkError객체 내의 변경된 함수 적용
    
    * 주석과 디버거 제거
    \

--- jsonParser.js
+++ jsonParser.js
@@ -5,16 +5,15 @@ class JSONData {
         this.child = child
     }
 }
-
-const sentence = "['1a'3',[22,23,[11,[112233],112],55],33]".replace(/ /gi, '')
+const sentence = "['1a3',[null,false,['11',[112233],{easy : ['hello', {a:''a'}, 'world']},112],55, '99'],{a:'str', b:[912,[5656,33],{key : 'innervalue', newkeys: [1,2,3,4,5]}]}, true]".replace(/ /gi, '')
 
 class Tokenize {
     constructor() {
         this.wholeDataQueue = [];
     }
-    
+
     getWholeDataQueue(sentence) {
-        while(sentence.length !== 0) {
+        while (sentence.length !== 0) {
             const token = this.getToken(sentence)
             this.wholeDataQueue.push(token)
             sentence = sentence.replace(token, '')
@@ -23,12 +22,16 @@ class Tokenize {
     }
 
     getToken(str) {
-        if (str[0] === '[' || str[0] === ',' || str[0] === ']') {
+        if (str[0] === '[' || str[0] === ',' || str[0] === ']' || str[0] === '{' || str[0] === '}') {
             return str.slice(0, 1)
         } else if (str.indexOf(']') < str.indexOf(',')) {
             return str.slice(0, str.indexOf(']'))
         } else if (str.indexOf(',') === -1) {
             return str.slice(0, str.indexOf(']'))
+        } else if (str.indexOf(':') !== -1 && str.indexOf(',') > str.indexOf(':')) {
+            return str.slice(0, str.indexOf(':') + 1)
+        } else if (str.indexOf('}') < str.indexOf(',')) {
+            return str.slice(0, str.indexOf('}'))
         } else {
             return str.slice(0, str.indexOf(','))
         }
@@ -40,28 +43,46 @@ class Analyze {
         this.queueArr = queue
         this.errorCheck = errorCheck
     }
-    
+
     queue() {
-        while(this.queueArr.length !== 0) {
+        while (this.queueArr.length !== 0) {
             const value = this.queueArr.shift()
-            if(value === '[') {
-                const child = this.getChild(this.queueArr, value)
-                return new JSONData('Array', 'Array Object', child)
+            if (value === '[') {
+                return this.makeArrayChild(this.queueArr, value)
+            } else if (value === '{') {
+                return this.makeObjectChild(this.queueArr, value)
             }
         }
     }
 
+    makeArrayChild(queueArr, value) {
+        const arrayChild = this.getChild(queueArr, value)
+        return new JSONData('Array', 'Array Object', arrayChild)
+    }
+
+    makeObjectChild(queueArr) {
+        const objectChild = this.getChild(queueArr, value)
+        return new JSONData('Object', 'Object Object', objectChild)
+    }
+
     getChild(queueArr, checkingValue) {
         let child = [];
+        debugger;
         while (checkingValue !== ']') {
             checkingValue = queueArr.shift()
-            if(checkingValue === '[') {
+            if (checkingValue === '[') {
                 child.push(new JSONData('Array', 'Object Array', this.getChild(queueArr, checkingValue)))
                 continue;
+            } else if (checkingValue === '{') {
+                child.push(new JSONData('Object', 'Object Object', this.getChild(queueArr, checkingValue)))
+                continue;
+            } else if (checkingValue.indexOf(':') !== -1) {
+                child.push(new JSONData('object key', checkingValue.slice(0, checkingValue.indexOf(':')), []))
+                continue;
+            } else if (checkingValue === '}' || checkingValue === ']') {
+                break;
             } else if (checkingValue === ',') {
                 continue;
-            } else if (checkingValue === ']') {
-                break;
             } else if (checkingValue === 'true' || checkingValue === 'false') {
                 child.push(new JSONData('Boolean', checkingValue, []))
                 continue;
@@ -69,47 +90,56 @@ class Analyze {
                 child.push(new JSONData('Null', checkingValue, []))
                 continue;
             } else if (checkingValue[0] === "'") {
-                if(this.errorCheck.checkString(checkingValue)) {
-                    console.log(`${checkingValue}는 제대로된 문자열이 아닙니다.`)
-                    return 
-                }
+                if (this.errorCheck.checkString(checkingValue)) return
                 child.push(new JSONData('String', checkingValue, []))
                 continue;
             }
-            if(this.errorCheck.checkNumber(checkingValue)) {
-                console.log(`${checkingValue}은 알수없는 문자열입니다.`)
-                return
-            }
+            if (this.errorCheck.checkNumber(checkingValue)) return
             child.push(new JSONData('Number', checkingValue, []))
         }
         return child
     }
-}
+};
 
-class ErrorCheck {    
-    checkString(token) {
-        debugger;
-        let count = 0
-        for(let position = 0; position < token.length; position++) {
-            if(token[position] === "'") {
-                count++
+class ErrorCheck {
+    countQueueNum(token) {
+        let quotesNum = 0
+        for(let position of token) {
+            if(position === "'") {
+                quotesNum++
             }
         }
-        if(count === 2 && token[0] === "'" && token[token.length-1] === "'") {
-            return false
-        }
-        return true
+        return quotesNum
     }
 
+    checkString(token) {
+        let quotesNum = this.countQueueNum(token)
+        if (quotesNum === 2 && token[0] === "'" && token[token.length - 1] === "'") {
+            return false
+        }
+        this.printErrorMessage('string', token)
+        return true
+    }
+    
     checkNumber(token) {
-        if(isNaN(Number(token)) === true) {
+        if (isNaN(Number(token))) {
+            this.printErrorMessage('number', token)
             return true
         }
         return false
     }
-}
 
-function printJSONData(JSONData) {
+    printErrorMessage(type, token) {
+        if(type === 'string') {
+            console.log(`${token}는 제대로된 문자열이 아닙니다.`)
+        }
+        if(type === 'number') {
+            console.log(`${token}은 알수없는 데이터입니다.`)
+        }
+    }
+};
+
+const print = function printJSONData(JSONData) {
     console.log(JSON.stringify(JSONData, null, 2))
 }
 
@@ -118,5 +148,5 @@ const tokenizedDataArr = tokenize.getWholeDataQueue(sentence)
 const errorCheck = new ErrorCheck
 const analyze = new Analyze(tokenizedDataArr, errorCheck)
 const jsonData = analyze.queue()
-printJSONData(jsonData)
+print(jsonData)
 
