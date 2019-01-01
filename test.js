af98d482f6c3a77f9d0647f8c65916bb3d361883
Hyeyoon Jung <hyeyoonjungkr@gmail.com>
2019-01-01 23:28:42 +0900

    expect, toBe 함수 구현

--- test.js
+++ test.js
@@ -10,3 +10,14 @@ const test = (name, fn) => {
     console.error('\x1b[41m\x1b[30m%s\x1b[0m', 'FAIL', error);
   }
 }
+
+const expect = value => {
+  return {
+    toBe(expect) {
+      if (JSON.stringify(value) === JSON.stringify(expect)) return true;
+      else throw Error(`${value}값과 ${expect}값이 일치하지 않습니다.`);
+    }
+  }
+}
+
+export { test, expect }
